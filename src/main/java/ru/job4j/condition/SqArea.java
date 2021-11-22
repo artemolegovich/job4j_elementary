150a3d4766a4a6817ee5171e3429b9732ba16dc3
artemolegovich <a.o.lepeshkin@gmail.com>
2021-11-22 19:41:45 +0300

    Отредактирован файл SqArea.java

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -2,15 +2,13 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(double p, double k) {
-        double rsl = p / (2 * (k + 1));
-        return rsl;
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        return l * h;
     }
 
     public static void main(String[] args) {
         double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
-        double l = result1 * 2;
-        double s = l * result1;
-        System.out.println("Площадь прямоугольника равна " + s);
+        System.out.println("Площадь прямоугольника равна " + result1);
     }
 }
