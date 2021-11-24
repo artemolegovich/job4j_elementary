9e8f7d62c77611ead6dfbc6ae314083d86b14eee
artemolegovich <a.o.lepeshkin@gmail.com>
2021-11-24 11:06:07 +0300

    Отредактирован файл SqAreaTest.java.

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -24,7 +24,7 @@ public class SqAreaTest {
     }
 
     @Test
-    public void whenP5K1dot5Then1dott5() {
+    public void whenP5K1dot5Then1dot5() {
         double expected = 1.5;
         int p = 5;
         double k = 1.5;
