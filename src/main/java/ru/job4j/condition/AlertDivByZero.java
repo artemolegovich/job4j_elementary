e7950becfdfa79ee521cf257acec70b4d3b9157a
artemolegovich <a.o.lepeshkin@gmail.com>
2021-11-24 11:08:53 +0300

    Отредактирован файл AlertDivByZero.java.

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -10,5 +10,8 @@ public class AlertDivByZero {
         if (number < 0) {
             System.out.println("This is negative number.");
         }
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
     }
 }
