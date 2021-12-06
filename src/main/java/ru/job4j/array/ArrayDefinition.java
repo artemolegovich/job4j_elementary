552aad9552026f5b5b205bdad405001a962ea33e
artemolegovich <a.o.lepeshkin@gmail.com>
2021-12-06 17:38:54 +0300

    Отредактирован файл ArrayDefinition.java.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        System.out.println();
+        String[] names = new String[4];
+        names[0] = "Andrey";
+        names[1] = "Ivan";
+        names[2] = "Anna";
+        names[3] = "Irina";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
